9bca9d408c3252718bfdb02cdb9e27b1424fb799
shkhuzae <shouqalkhuzaei@gmail.com>
2019-01-31 18:16:21 +0000

    Last commit

--- SinatraLab.rb
+++ SinatraLab.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
 get '/' do
+   erb :application do
     erb :index
+   end
+end
+
+post '/' do
+@fname = params[:firstname]
+@lname = params[:lastname]
+@class_love = params[:class_love]
+@feedback = params[:feedback]
+@happy = (@class_love == "More than I care to say") ? "That's Great!" : "Well, hopefully
+you'll REALLY love it by the end"
+erb :application do
+erb :response
+end
 end
